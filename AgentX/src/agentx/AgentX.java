cbe96474b7407bec61cdba32cadca3428eb40a78
Wall-Jordan <wall.jordan@live.com>
2015-01-30 10:35:28 -0700

    game class completed and initialized

--- AgentX/src/agentx/AgentX.java
+++ AgentX/src/agentx/AgentX.java
@@ -5,6 +5,7 @@
  */
 
 package agentx;
+import agentx.model.Game;
 import agentx.model.Player;
 
 
@@ -25,6 +26,14 @@ public class AgentX {
         
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
+        
+        Game gameOne = new Game();
+        
+        gameOne.setGameName("Agent Fred");
+        gameOne.setSaveTime("1.30.15 23:59");
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
     }
     
 }
