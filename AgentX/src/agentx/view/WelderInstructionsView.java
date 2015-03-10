132b02c710fc0abb48bc4a11c1be79c811d9396e
Wall-Jordan <wall.jordan@live.com>
2015-03-10 09:50:55 -0600

    WelderInstructionsView

--- AgentX/src/agentx/view/WelderInstructionsView.java
+++ AgentX/src/agentx/view/WelderInstructionsView.java
@@ -15,26 +15,27 @@ import java.util.Scanner;
 public class WelderInstructionsView extends View {
 
     public WelderInstructionsView() {
-        super("\n"
-                + "\n\t\t\t---Shop Teachers Office---"
-                + "\n"
-                + "\n\tYou have now entered the office of Mr. Schwartzenbeger, who was"
-                + "\n\tthe shop teacher of Slagem High School."
-                + "\n"
-                + "\n\tAs you explore Mr. Schwartzenbeger's office you discover a set"
-                + "of instructions for the welder you found in the shop."
-                + "\n"
-                + "\n\tThe instructions said:"
-                + "\n\t\t\tHow to use your Edison 5000:"
-                + "\n\t\tThe Edison 5000 is the top of the line welder that is ran by"
-                + "\n\t\ta computer, which makes it very simple to use."
-                + "\n\t\t1.)Choose what metal object needs welded"
-                + "\n\t\t2.)You simply type in \"T2\" and then you type in the name of"
-                + "\n\t\t object the you want welded such as \"ship\""
-                + "\n"
-                + "\n\tNow it is your turn to try it!"
-        );
+        super(" ");
     }
+
+    private final String WELD_INSTRUCTIONS = "\n"
+            + "\n\t\t\t---Shop Teachers Office---"
+            + "\n"
+            + "\n\tYou have now entered the office of Mr. Schwartzenbeger, who was"
+            + "\n\tthe shop teacher of Slagem High School."
+            + "\n"
+            + "\n\tAs you explore Mr. Schwartzenbeger's office you discover a set"
+            + "\n\tof instructions for the welder you found in the shop."
+            + "\n"
+            + "\n\tThe instructions said:"
+            + "\n\t\t\tHow to use your Edison 5000:"
+            + "\n\t\tThe Edison 5000 is the top of the line welder that is ran by"
+            + "\n\t\ta computer, which makes it very simple to use."
+            + "\n\t\t1.)Choose what metal object needs welded"
+            + "\n\t\t2.)You simply type in \"T2\" and then you type in the name of"
+            + "\n\t\t object the you want welded such as \"ship\""
+            + "\n"
+            + "\n\tNow it is your turn to try it!";
 
     private final String WELD = "\tIn the office you found a few metal pieces that"
             + "\n\tcame from a puzzle."
@@ -43,6 +44,8 @@ public class WelderInstructionsView extends View {
 
     @Override
     public void display() {
+        System.out.println(WELD_INSTRUCTIONS);
+        
         String weldInput = " ";
 
         boolean welded = false;
@@ -74,7 +77,7 @@ public class WelderInstructionsView extends View {
 
         return weld;
     }
-    
+
     @Override
     public boolean doAction(Object obj) {
         String weldInput = (String) obj;
