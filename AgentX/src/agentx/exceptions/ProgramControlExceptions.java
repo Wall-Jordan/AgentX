a2bfef08f0bfc290670733673214de29a8ce8d51
MatMik12 <mik12006@byui.edu>
2015-03-20 16:20:52 -0600

    Merge origin/master
    Conflicts:
    	AgentX/src/agentx/view/GameMenuView.java

---
+++ AgentX/src/agentx/exceptions/ProgramControlExceptions.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agentx.exceptions;
+
+/**
+ *
+ * @author matiasmikkola
+ */
+public class ProgramControlExceptions extends Exception{
+
+    public ProgramControlExceptions() {
+    }
+
+    public ProgramControlExceptions(String message) {
+        super(message);
+    }
+
+    public ProgramControlExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ProgramControlExceptions(Throwable cause) {
+        super(cause);
+    }
+
+    public ProgramControlExceptions(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    
+}
