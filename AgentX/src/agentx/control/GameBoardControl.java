c513b6549ddec0f6b42d0827e9c3dd72703b5c06
science-guy <aztechanna@hotmail.com>
2015-04-01 17:46:37 -0600

    Merge origin/master
    Conflicts:
    	AgentX/src/agentx/view/ListToolView.java

--- AgentX/src/agentx/control/GameBoardControl.java
+++ AgentX/src/agentx/control/GameBoardControl.java
@@ -18,32 +18,31 @@ import java.io.PrintWriter;
  * @author Chris
  */
 public class GameBoardControl {
-    
+
     protected final static PrintWriter console = AgentX.getOutFile();
+
     public static GameBoard createGameBoard() throws GameBoardControlExceptions {
         console.println("called createGameboard");
         return null;
     }
-    
+
     public static void moveActorsToStartingLocation(GameBoard gameBoard) {
         console.println("Move actors to starting locatinon");
     }
-    
+
     private static void assignScenesToLocations(GameBoard gameBoard, Scene[] scenes) {
         Location[][] locations = gameBoardLocations();
-        
-        
-        
+
     }
 
     private static Location[][] gameBoardLocations() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
     private static void assignScenesToBlock(GameBoard gameBoard, Scene[] scenes) {
         Location[][] locations = gameBoard.getLocations();
-        
-     //   locations[0][0].setBlock(scenes[SectionOne.drill.ordinal()]);
+
+        //   locations[0][0].setBlock(scenes[SectionOne.drill.ordinal()]);
     }
+   
 }
