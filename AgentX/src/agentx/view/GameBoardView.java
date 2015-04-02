626c767146e1fd63b5d042240c15d80aa255dd9d
science-guy <aztechanna@hotmail.com>
2015-04-02 17:53:18 -0600

    Merge origin/master
    Conflicts:
    	AgentX/src/agentx/control/GameBoardControl.java

--- AgentX/src/agentx/view/GameBoardView.java
+++ AgentX/src/agentx/view/GameBoardView.java
@@ -6,36 +6,293 @@
 package agentx.view;
 
 import agentx.AgentX;
+import agentx.control.GameBoardControl;
+import agentx.model.Location;
+import agentx.view.ViewInterface.View;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 /**
  *
  * @author matiasmikkola
  */
-public class GameBoardView {
-protected final static PrintWriter console = AgentX.getOutFile();
-    void display() {
-        char gameBoard[][] = new char[3][10];
+public class GameBoardView extends View {
+    ArrayList<Location> locations = GameBoardControl.locations;
+    public GameBoardView() {
+        super("");
+    }
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 10; j++) {
-                gameBoard[i][j] = 'X';
-            }
+    private void displayGameBoard(){
+        char loc0;
+        char loc1;
+        char loc2;
+        char loc3;
+        char loc4;
+        char loc5;
+        char loc6;
+        char loc7;
+        char loc8;
+        char loc9;
+        char loc10;
+        char loc11;
+        char loc12;
+        char loc13;
+        char loc14;
+        char loc15;
+        char loc16;
+        char loc17;
+        char loc18;
+        char loc19;
+        char loc20;
+        char loc21;
+        char loc22;
+        char loc23;
+        char loc24;
+        char loc25;
+        char loc26;
+        char loc27;
+        char loc28;
+        char loc29;
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
         }
-        
-        console.println("                           Agent X Gameboard          ");
-        console.println("                 1   2   3   4   5   6   7   8   9   10");
-        console.println("                 -   -   -   -   -   -   -   -   -   - ");
-        for (int i = 0; i < 3; i++) {
-            console.print("Time Period " + (i+1) + ": | ");
-            for (int j = 0; j < 10; j++) {
-                console.print(gameBoard[i][j]);
-                console.print(" | ");
-            }
-            console.print("\n");
-            console.println("                 -   -   -   -   -   -   -   -   -   - ");
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
         }
-        
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+        if(locations.get(0).getVisited() == true && locations.get(0).getActive() == false){
+            loc0 = 'V';
+        }else if(locations.get(0).getVisited() == true && locations.get(0).getActive() == true){
+            loc0 = 'O';
+        }else{
+            loc0 = 'X';
+        }
+
     }
     
+//    void display() {
+//        console.println();
+//        char gameBoard[][] = new char[3][10];
+//
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 10; j++) {
+//                gameBoard[i][j] = 'X';
+//            }
+//        }
+//
+//        console.println("                           Agent X Gameboard          ");
+//        console.println("                 1   2   3   4   5   6   7   8   9   10");
+//        console.println("                 -   -   -   -   -   -   -   -   -   - ");
+//        for (int i = 0; i < 3; i++) {
+//            console.print("Time Period " + (i + 1) + ": | ");
+//            for (int j = 0; j < 10; j++) {
+//                console.print(gameBoard[i][j]);
+//                console.print(" | ");
+//            }
+//            console.print("\n");
+//            console.println("                 -   -   -   -   -   -   -   -   -   - ");
+//        }
+//
+//    }
+
+    @Override
+    public boolean doAction(Object obj) {
+        return false;
+    }
 }
