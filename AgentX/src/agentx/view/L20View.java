051dd07ea66d701f61ceadb090edf98563580782
MatMik12 <mik12006@byui.edu>
2015-04-02 15:17:34 -0600

    Merge origin/master
    Conflicts:
    	AgentX/src/agentx/control/GameBoardControl.java

--- AgentX/src/agentx/view/L20View.java
+++ AgentX/src/agentx/view/L20View.java
@@ -5,12 +5,37 @@
  */
 package agentx.view;
 
+import static agentx.control.GameBoardControl.createLocations;
+import agentx.model.Location;
+import java.util.ArrayList;
+
 /**
  *
  * @author matiasmikkola
  */
-public class L20View {
+public class L20View extends ViewInterface.View {
+
+    public L20View() {
+        super("You have landed at a what seems to be a tropical forest. You should probably"
+                + "explore and calculate the amount of fuel you have.\n"
+                + "\n****************************************"
+                + "\nTL - Display to do list"
+                + "\nO  - Other commands menu"
+                + "\n****************************************\n");
+    }
     
-    L20.getToDoList();
-    
+    @Override
+    public boolean doAction(Object obj){
+        ArrayList<Location> locations = createLocations();
+        String input = (String) obj;
+        
+        switch(input){
+            case "TL":
+                break;
+            case "O":
+                break;
+        }
+        
+        return true;
+    }
 }
--- AgentX/src/agentx/view/L21View.java
+++ AgentX/src/agentx/view/L21View.java
@@ -5,10 +5,37 @@
  */
 package agentx.view;
 
+import static agentx.control.GameBoardControl.createLocations;
+import agentx.model.Location;
+import java.util.ArrayList;
+
 /**
  *
  * @author matiasmikkola
  */
-public class L21View {
+public class L21View extends ViewInterface.View {
+
+    public L21View() {
+        super("You moved into an old village, which is empty. You move closer, and notice"
+                + "that there are bodies everywhere. Maybe this is where Chaotica is right now!\n"
+                + "\n****************************************"
+                + "\nTL - Display to do list"
+                + "\nO  - Other commands menu"
+                + "\n****************************************\n");
+    }
     
+    @Override
+    public boolean doAction(Object obj){
+        ArrayList<Location> locations = createLocations();
+        String input = (String) obj;
+        
+        switch(input){
+            case "TL":
+                break;
+            case "O":
+                break;
+        }
+        
+        return true;
+    }
 }
