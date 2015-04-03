86b2a1a9567d42ce740b652c87132f4ccbc9d5b9
MatMik12 <mik12006@byui.edu>
2015-04-03 11:27:57 -0600

    Merge origin/master
    Conflicts:
    	AgentX/src/agentx/view/L10View.java

--- AgentX/src/agentx/view/L29View.java
+++ AgentX/src/agentx/view/L29View.java
@@ -1,8 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+////
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
 package agentx.view;
 
 import agentx.control.GameBoardControl;
@@ -11,6 +11,7 @@ import agentx.control.InventoryControl;
 import static agentx.control.InventoryControl.getDrillBit;
 import agentx.control.PuzzleControl;
 import agentx.exceptions.PuzzleControlExceptions;
+import agentx.model.FuelContainer;
 import agentx.model.Location;
 import static agentx.view.L20View.ship3;
 import java.util.ArrayList;
@@ -29,8 +30,26 @@ public class L29View extends ViewInterface.View {
                 + "\nO  - Other commands menu"
                 + "\nI  - Instructions"
                 + "\nV  - View Gameboard"
+                + "\nF  - Check Fuel"
+                + "\nTAKEOFF  - Take Off"
                 + "\n****************************************\n");
     }
+
+//    public String getInput() {
+//        String year = null;
+//
+//        this.console.println("Destination year:");
+//
+//        try {
+//            year = this.keyboard.readLine();
+//            year = year.trim();
+//            year = year.toUpperCase();
+//        } catch (Exception e) {
+//            this.console.println("Error reading input: " + e.getMessage());
+//        }
+//
+//        return year;
+//    }
 
     @Override
     public boolean doAction(Object obj) {
@@ -53,9 +72,6 @@ public class L29View extends ViewInterface.View {
                 instructionsView.display();
             case "V":
                 return true;
-            case "TAKEOFF":
-                TakeOffView takeOffView = new TakeOffView();
-                takeOffView.display();
             case "T4":
                 try {
                     String drillBit = getDrillBit();
@@ -73,10 +89,60 @@ public class L29View extends ViewInterface.View {
                 } catch (PuzzleControlExceptions pce) {
                     ErrorView.display("L21View.java", pce.getMessage());
                 }
-
-                break;
+                
+                return false;
         }
-
+        
         return false;
+        
     }
 }
+    
+
+//            case "TAKEOFF":
+//                int shipFixed = ship3.getStatus();
+//                FuelContainer shipFuel = ship3.getFuel();
+//                if (shipFixed < 1) {
+//                    console.println("You can not take off. You dont have enough fuel.");
+//                    return true;
+//                } else {
+//                    String year = " ";
+//                    boolean traveled = false;
+//                    String goBack = " ";
+//
+//                    do {
+////                        String input = this.getInput();
+//                        year = input;
+//
+//                        traveled = this.doAction(year);
+//
+//                        if (traveled == false) {
+//                            goBack = this.getInput2();
+//                        } else {
+//                            goBack = "Y";
+//                        }
+//                    } while (!"Y".equals(goBack));
+//
+//                    if (traveled == true) {
+//                        this.console.println("You are now in" + year + ".");
+//                    } else {
+//                        this.console.println("Go back and look for a year clue.");
+//                    }
+//                    break;
+//                }
+//        }
+//    
+//        private String getInput2() {
+//        String selection = null;
+//
+//        this.console.println("Would you like to go back? [Y, N]");
+//        try {
+//            selection = this.keyboard.readLine();
+//            selection = selection.trim();
+//            selection = selection.toUpperCase();
+//        } catch (Exception e) {
+//            ErrorView.display("TakeOffView.java", "Error reading input: " + e.getMessage());
+//        }
+//        return selection;
+//    }
+//}
