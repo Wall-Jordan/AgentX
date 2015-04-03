9c16d81a35234d755da9ddd9475338b7767742d2
MatMik12 <mik12006@byui.edu>
2015-04-03 10:26:13 -0600

    Merge origin/master
    Conflicts:
    	AgentX/src/agentx/view/L10View.java

--- AgentX/src/agentx/view/GameEndView.java
+++ AgentX/src/agentx/view/GameEndView.java
@@ -14,29 +14,47 @@ import agentx.view.ViewInterface.View;
 public class GameEndView extends View {
     
     public GameEndView() {
-        super("\n"
-                + "**********************************\n"
-                + "* CONGRATULATIONS, YOU HAVE WON! *\n"
-                + "**********************************");
+        super("");
     }
     
-//    @Override
-//    
-//    public boolean doAction(Object obj) {
-//        String value = (String) obj;
-//
-//        value = value.toUpperCase();
-//        char choice = value.charAt(0);
-//
-//        switch (choice) {
-//            case 'E':
-//                this.displayExit();
-//                break;
-//            default:
-//                console.println("\n Invalid selection, try again!");
-//        }
-//        return true;
-//    }
+    @Override
+        public void display() {
+            String value;
+            boolean done = false;
+
+            do {
+
+                console.println("\n"
+                + "***************************************\n"
+                + "* CONGRATULATIONS, YOU HAVE DEFEATED  *\n"
+                + "*     CHAOTICA, AND WON THE GAME!     *\n"
+                + "*         GOOD JOB AGENT!!           *\n"
+                + "***************************************");
+                value = this.getInput();
+                done = this.doAction(value);
+
+            } while (!done);
+        }
+    
+    
+    
+    @Override
+    
+    public boolean doAction(Object obj) {
+        String value = (String) obj;
+
+        value = value.toUpperCase();
+        char choice = value.charAt(0);
+
+        switch (choice) {
+            case 'E':
+                this.displayExit();
+                break;
+            default:
+                console.println("");
+        }
+        return true;
+    }
 
     private boolean displayExit() {
         return false;
